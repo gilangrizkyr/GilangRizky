a7bfe623e339844dd13505833b7cf5938d8e6e11
Gilang Rizky <gilangrizkyramadhan34@gmail.com>
2026-03-12 11:54:44 +0800

    Fix Undefined Variable navGH in navbar.php

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -2,6 +2,7 @@
     <a href="<?= base_url('/') ?>" class="navbar-logo" aria-label="Home">G.</a>
     <ul class="navbar-links">
         <li><a href="<?= base_url('/#works') ?>">Projects</a></li>
+        <?php $navGH = (isset($bio) && (is_array($bio) ? ($bio['github_url'] ?? '#') : ($bio->github_url ?? '#'))) ?: '#'; ?>
         <li><a href="<?= esc($navGH) ?>" target="_blank" rel="noopener">GitHub</a></li>
     </ul>
 </nav>
